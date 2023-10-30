b61860e8cb616eeb2b7319ef70cfaea3568d818f
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 13:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 let table=document.querySelector("table");
 	let tr=document.createElement("tr");
+    tr.setAttribute(id, "ans");
 	table.appendChild(tr);
 const getSum = () => {
 		let arr=document.querySelectorAll(".price");
